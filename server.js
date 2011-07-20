393ef16b3a3be2d0d8007500cbf3326f248944d2
Brian Hollenbeck <brian@elsewares.org>
2011-07-20 18:51:37 -0500

    Changed the port to 80 for Joyent.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ CHODE.server = http.createServer(function (req, res){
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('This is chode speaking.');
   console.log('Request completed to ' + req.headers);
-  }).listen(8080, "127.0.0.1");
+  }).listen(80, "127.0.0.1");
 
-console.log('Chode server running at localhost:8080');
+console.log('Chode server running at localhost:80');
 
